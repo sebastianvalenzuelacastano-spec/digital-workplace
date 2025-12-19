86a97b8b274bc1a3167e00561e271c116f892a64
Sebastian Valenzuela <sebapc@mac.lan>
2025-12-18 21:19:07 -0700

    Revert to standard next start to fix CSS loading issues

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+
   typescript: {
     // Also ignore typescript errors for now to ensure we can build what we have running in dev
     ignoreBuildErrors: true,
